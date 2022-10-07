d4d77e24a61353c0c54e31aea31434a6790e473a
Andrew <andrey777-novikov777@yandex.ru>
2022-10-07 08:33:59 +0300

    +auth ui (v1)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('just testing.')
+import sys
+
+from PyQt5 import uic, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('uis/Authorization.ui', self)
+        #  self.setWindowFlag(QtCore.Qt.FramelessWindowHint) - Убрать рамку сверху
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
