c35e26f983ced098f1a168b53f394fcc9229f504
Andrew <andrey777-novikov777@yandex.ru>
2022-10-12 18:56:24 +0300

    quick fix

--- mainPage.py
+++ mainPage.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
-from PyQt5 import uic
+from PyQt5 import uic, QtGui
 
 
 class mainPage(QMainWindow):
@@ -11,6 +11,7 @@ class mainPage(QMainWindow):
         uic.loadUi('uis\mainPage.ui', self)
         self.setWindowFlag(Qt.FramelessWindowHint)
 
+        # self.setAttribute(Qt.WA_NoSystemBackground)
         self.exit.clicked.connect(lambda: app.exit())
         self.minimize.clicked.connect(lambda: self.showMinimized())
         self.controlPanel.mouseMoveEvent = self.moveWindow
@@ -28,5 +29,6 @@ class mainPage(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = mainPage()
+    window.setWindowIcon(QtGui.QIcon('icons/app.png'))
     window.show()
     sys.exit(app.exec_())
