50c8de9dc7bef82f9c8d4ad9afa68bdc017cd46d
Andrew <andrey777-novikov777@yandex.ru>
2022-10-16 10:29:47 +0300

    Finally added the ability to switch between uis by clicking the button

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import sys
 
-import time
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
 from downloadPage import downloadWindow
 from mainPage import mainPage
-from register import RegistrationWindow
+
+
+def switch(_from, _to):
+    # Math at it's finest. My own creation =)
+    lastPos = _from.pos()
+    modifiedPos = (lastPos.x() + _from.width() // 2,
+                   lastPos.y() + _from.height() // 2)
+    newPos = (modifiedPos[0] - _to.width() // 2,
+              modifiedPos[1] - _from.height() // 2)
+
+    _to.move(*newPos)
+    _to.show()
+    _from.hide()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window1 = mainPage()
-    window2 = downloadWindow()
-    window1.show()
-    window2.show()
-    print(app.activeWindow())
+    mainWin = mainPage()
+    downloadWin = downloadWindow()
+    mainWin.show()
+
+    mainWin.downloadButton.clicked.connect(
+        lambda: switch(mainWin, downloadWin))
+    downloadWin.returnButton.clicked.connect(
+        lambda: switch(downloadWin, mainWin))
+
     sys.exit(app.exec_())
