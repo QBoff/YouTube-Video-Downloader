394c97a1d5846b69bc919a30d401fdfe3641b758
Dany <86149558+QBoff@users.noreply.github.com>
2022-11-02 23:28:57 +0300

    added the ability to download video, audio and subtitles in multithreaded mode

--- downloadPage.py
+++ downloadPage.py
@@ -1,14 +1,14 @@
 import sys
-from os.path import join
-
+from os.path import join, splitext
+from os import rename
 from PyQt5.QtWidgets import QApplication, QMainWindow, QSizeGrip
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 from youtube import getVideoInfo
 from pytube import YouTube
-from multiprocessing import Process
 from threading import Thread
+from youtube_transcript_api import YouTubeTranscriptApi
 
 
 class DownloadPage(QMainWindow):
@@ -33,18 +33,20 @@ class DownloadPage(QMainWindow):
             grip.resize(self.gripSize, self.gripSize)
             self.grips.append(grip)
 
-        self.downloadButton.clicked.connect(self.download)
-    
+        self.downloadButton.clicked.connect(self.download_video)
+
     # the logic of this page begins
-    def _download_video(self) -> str:
+    def _download_video_or_audio(self) -> str:
 
         self.link = self.urlInput.text()
         self.resolution = self.qualityInput.currentText()  # get resolution from Interface
         self.extension = self.comboBox_2.currentText()  # get extension from Interface
-        self.path_to_save = "downloaded_video/"
+        self.path_to_save_video = join("downloaded_video")
+        self.path_to_save_audio = join("downloaded_audio")
 
         try:
-            yt = YouTube(self.link)  # if link is correct and this video is on youtube
+            # if link is correct and this video is on youtube
+            yt = YouTube(self.link)
         except:
             print("Link isn't valid")
 
@@ -53,20 +55,50 @@ class DownloadPage(QMainWindow):
 
         self.sizeText.setText(f"Estimated size: {str(round(video_size, 2))}GB")
 
-        mp4video = yt.streams.filter(
-            file_extension=self.extension, res=self.resolution)
-        
-        try:    
-            mp4video.first().download(self.path_to_save)
-        except:
-            print("Downloading error")
-        print("ok")
-    
-    def download(self):
+        if self.extension == "mp4":
+            mp4video = yt.streams.filter(
+                file_extension=self.extension, res=self.resolution)
+            try:
+                mp4video.first().download(self.path_to_save_video)
+            except:
+                print("Downloading error")
+
+        elif self.extension == "mp3":
+            mp3audio = yt.streams.filter(only_audio=True)
+            try:
+                audio = mp3audio.first().download(self.path_to_save_audio)
+                base, ext = splitext(audio)
+                new_file = base + '.mp3'
+                rename(audio, new_file)
+            except:
+                print("Download error")
+        elif self.extension == "str":
+            data = YouTubeTranscriptApi.get_transcript(
+                self.url_processign(self.link),
+                ('en', 'ru')
+            )
+            name_for_file = "sub_file"
+            with open(f"downloaded_subtitles/{name_for_file}.str", "w", encoding="utf-8") as file:
+                
+                for item in data:
+                    # if item starts with [ it's not our stuff :)
+                    print(item)
+                    if item["start"] < 60:
+                        time = f'{str(item["start"])} сек'
+                    elif item["start"] >= 60 and item["start"] < 3600:
+                        time = f'{item["start"] // 60} мин {str(item["start"] % 60)} сек'
+                    elif item["start"] >= 3600:
+                        time = f'{item["start"] // 3600} час {item["start"] % 3600 // 60} мин {item["start"] % 60} сек'
+                    if item["text"][0] != '[':
+                        file.write(item["text"] + " |" + time + "|" + "\n")
+
+    def download_video(self):
         Thread(
-            target=self._download_video, daemon=True
+            target=self._download_video_or_audio, daemon=True
         ).start()
-        
+
+    def url_processign(self, url) -> str:
+        return "=".join(url.split('=')[1:])
 
     def onURLtype(self) -> None:
         url = self.urlInput.text()
