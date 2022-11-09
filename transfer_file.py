7390c9c01540a00980f164c228668addb22d9b75
Dany <86149558+QBoff@users.noreply.github.com>
2022-11-09 21:46:49 +0300

    add file transfer

--- transfer_file.py
+++ transfer_file.py
@@ -1,26 +1,54 @@
-import socket
-import argparse
+# for implementing the HTTP Web servers
 import http.server
+# provides access to the BSD socket interface
+import socket
+# a framework for network servers
 import socketserver
+# to display a Web-based documents to users
+import webbrowser
+# to generate qrcode
+import pyqrcode
+from pyqrcode import QRCode
+
+import png  # for converting into png format
+import os
+
+from datamanager import Manager, Profile
 
 
-parser = argparse.ArgumentParser(description='Serve files from the current \
-                                 directory.')
+PORT = 8010
+# it will work when i will connect it with main.py
+# account = Manager.getActiveUser()
+# pr = Profile(account.userLogin)
+# print(pr.settings)
 
-host_name = socket.gethostname()
-ip = socket.gethostbyname(host_name)
+user_path = os.path.join(os.getcwd(), "qboff\\Videos")
+os.chdir(user_path)
 
-parser.add_argument('--host', default=ip, type=str, required=False,
-                    help='Specify the ip address to listen on.')
+Handler = http.server.SimpleHTTPRequestHandler
 
-parser.add_argument('--port', default=8080, type=int, required=False,
-                    help='Specify the port to listen on.')
+hostname = socket.gethostname()
 
-args = parser.parse_args()
+# finding the IP address of the PC
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
+link = IP
 
-handler = http.server.SimpleHTTPRequestHandler
+# converts the IP address into a Qrcode
+url = pyqrcode.create(link)
+# saves the Qrcode inform of svg
+url.svg("myqr.svg", scale=8)
+# opens the Qrcode image in the web browser
+webbrowser.open('myqr.svg')
 
 
-with socketserver.TCPServer((args.host, args.port), handler) as httpd:
-    print(f'Server is listening on {args.host} on port {args.port}.')
+# Creating the HTTP request and serving the
+# folder in the PORT 8010,and the pyqrcode is generated
+ 
+# continuous stream of data between client and server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    print("Type this in your Browser", IP)
+    print("or Use the QRCode")
     httpd.serve_forever()
